2d1103ba8f3ecdf75e216fcfe534d5acc8e3847b
Ehsan Momeni Bashusqeh <emomeni1371@hotmail.com>
2024-02-19 10:11:05 +0330

    Update showmac.py

--- ShowMAC/showmac.py
+++ ShowMAC/showmac.py
@@ -5,22 +5,22 @@ from rich import print
 
 nr = InitNornir(config_file="config.yaml")
 
-# user will insert the specific mac address in the correct format, it can be inserted in partial format
+# user should insert the specific mac address in the correct format, it can be inserted in partial format.
 macaddress = input("write down your mac address here...")
 print("                Vlan      MAC_Address     Age_Time(when exists)    Port")
 
 
-# the function that want to find the mac address on all ports that it has been learned
+# the function is going to find the mac address on all interfaces that it has been learnt and show "device name: vlan number, mac address, age time and configuration of interfaces” as a result.
 def showmac(task):
    try:
-   # getmac variable will store the result of "show mac address-table" command
+   # getmac variable will store the result of "show mac address-table" command.
     getmac = task.run(task=netmiko_send_command, command_string=f"sh mac address-table | inc  {macaddress}")
     task.host["facts"] = getmac.result
     
     # devicemac variable will store the facts of getmac variable
     devicemac = task.host["facts"]
 
-    # here we will replace the unwanted sections
+    # we will replace the unwanted sections of devicemac variable
     if devicemac != "":
      devicemac=devicemac.replace("dynamic","")
      devicemac=devicemac.replace("DYNAMIC","")
@@ -28,15 +28,15 @@ def showmac(task):
      devicemac=devicemac.replace("~~~","")
      devicemac=devicemac.replace(" ","-")
 
-     # deviceint variable will store the related interfaces that has been learned the mac address
+     # deviceint variable will store the related interfaces that have learnt the mac address.
      deviceint=devicemac[-10:]
      deviceint=deviceint.replace("-","")
 
-     # getint variable will store the interfaces' configurations that has learned the specific mac address that the user has been inserted
+     # getint variable will store the configuration of interfaces that have learnt the specific mac address that the user has been inserted.
      getint = task.run(task=netmiko_send_command, command_string=f"sh run int {deviceint}")
      task.host["facts"] = getint.result
 
-     # intdescription will store the facts of getint variable
+     # intdescription variable will store the facts of getint variable.
      intdescription = task.host["facts"]
      if "Invalid" in intdescription:
         intdescription= "That is not physical interface"
